61163539982d34d355c4f2ee02e2bdc529ebe52a
Gojo Satoro <vladimirkul06@gmail.com>
2025-01-14 14:44:39 +0200

    Admin panel setup

--- app_/carts/admin.py
+++ app_/carts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Cart
+from carts.models import Cart
 # Register your models here.
 
-admin.site.register(Cart)
+# admin.site.register(Cart)
+
+class CartTabAdmin(admin.TabularInline):
+    model = Cart
+    fields = 'product', 'qty', 'created_timestamp'
+    search_fields = 'product__name','qty','created_timestamp'
+    readonly_fields = ('created_timestamp',)
+    extra = 1
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user_display', 'product_display', 'qty', 'created_timestamp',]
+    list_filter = ['created_timestamp', 'user', 'product__name',]
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return 'Анонимный пользователь'
+
+    def product_display(self, obj):
+        return str(obj.product.name)
--- app_/goods/models.py
+++ app_/goods/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Categories(models.Model):
@@ -26,6 +27,10 @@ class Products(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
     
     class Meta:
         db_table = 'product'
